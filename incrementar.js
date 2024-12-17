cd197a4bdce90156b898806e43409b04accac129
jorge0404 <50037183+jorge0404@users.noreply.github.com>
2024-12-17 10:27:05 -0500

    Update incrementar.js

--- incrementar.js
+++ incrementar.js
@@ -1,1 +1,11 @@
+// Creamos una variable para almacenar el número
+let numero = 0;
 
+// Función para incrementar el número
+function incrementarNumero() {
+  numero++;
+  console.log("El número ahora es:", numero);
+}
+
+// Llamamos a la función para incrementar el número
+incrementarNumero();
